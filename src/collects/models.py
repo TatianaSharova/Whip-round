20ebded61dd64da856f7a74996c183eb95a7352d
Татьяна Шарова <tanyasharova2001@mail.ru>
2025-04-23 16:37:04 +0400

    feat/ first ver Collect ans Payments models

--- src/collects/models.py
+++ src/collects/models.py
@@ -1,6 +1,10 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.db.models import Sum
 
-from config.constants import REASON_TYPES
+from config.constants import (COLLECT_MAX_LENGTH, MAX_LENGTH_REASON,
+                              MAX_PAYMENT_AMOUNT, MIN_PAYMENT_AMOUNT,
+                              REASON_TYPES)
 from users.models import User
 
 
@@ -8,9 +12,10 @@ class Collect(models.Model):
     """
     Модель группового сбора.
     """
-    title = models.CharField('Название', max_length=300)
-    description = models.TextField('Описание', max_length=300)
-    reason = models.CharField('Повод', choices=REASON_TYPES)
+    title = models.CharField('Название', max_length=COLLECT_MAX_LENGTH)
+    description = models.TextField('Описание', max_length=COLLECT_MAX_LENGTH)
+    reason = models.CharField('Повод', max_length=MAX_LENGTH_REASON,
+                              choices=REASON_TYPES)
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
     end_date = models.DateTimeField('Дата завершения сбора',
                                     null=True, blank=True)
@@ -18,9 +23,7 @@ class Collect(models.Model):
         'Цель сбора', null=True, blank=True,
         help_text='Оставьте пустым для бесконечного сбора'
     )
-    collected_amount = models.PositiveIntegerField('Собранная сумма',
-                                                   default=0)
-    image = models.ImageField('Обложка', upload_to='collects/',
+    image = models.ImageField('Обложка', upload_to='collects/images/',
                               null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='collects',
@@ -33,6 +36,20 @@ class Collect(models.Model):
     def __str__(self):
         return self.title
 
+    @property
+    def payments_count(self) -> int:
+        """
+        Возвращает количество проведенных платежей для сбора.
+        """
+        return self.payments.count()
+
+    @property
+    def collected_amount(self) -> int:
+        """
+        Возвращает сумму всех платежей для сбора.
+        """
+        return self.payments.aggregate(total=Sum('amount'))['total'] or 0
+
 
 class Payment(models.Model):
     """
@@ -44,7 +61,9 @@ class Payment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='payments',
                                verbose_name='Автор')
-    amount = models.PositiveIntegerField('Пожертвование')
+    amount = models.PositiveIntegerField(
+        'Сумма платежа', validators=[MinValueValidator(MIN_PAYMENT_AMOUNT),
+                                     MaxValueValidator(MAX_PAYMENT_AMOUNT)])
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
 
     class Meta:
