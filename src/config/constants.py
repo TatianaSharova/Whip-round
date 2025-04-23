e80c3ede9f3b7a21b8f8047c5be47f23b0236707
Татьяна Шарова <tanyasharova2001@mail.ru>
2025-04-23 16:35:43 +0400

    add new constants for models and emails

--- src/config/constants.py
+++ src/config/constants.py
@@ -15,7 +15,19 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
 
-REASON_TYPES = ('birthday', 'Birthday',
-                'wedding', 'Wedding',
-                'charity', 'Charity',
-                'other', 'Other')
+REASON_TYPES = (
+    ('birthday', 'День рождения'),
+    ('wedding', 'Свадьба'),
+    ('charity', 'Благотворительность'),
+    ('other', 'Другое'),
+)
+MAX_LENGTH_REASON = 20
+COLLECT_MAX_LENGTH = 300
+
+MAX_PAYMENT_AMOUNT = 100000000000
+MIN_PAYMENT_AMOUNT = 1
+
+COLLECT_SUB_TEXT = 'Ваш групповой сбор был успешно создан'
+COLLECT_MESSAGE_TEXT = 'Ваш сбор был успешно создан! Начните собирать средства.'
+PAYMENT_SUB_TEXT = 'Ваш платёж успешно прошел'
+PAYMENT_MESSAGE_TEXT = 'Спасибо за пожертвование, ваш платёж успешно прошел!'
