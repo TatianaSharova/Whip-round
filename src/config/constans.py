f1c06fb59ce385538bf7df4585d3720a45aa1c85
Татьяна Шарова <tanyasharova2001@mail.ru>
2025-04-24 11:04:12 +0400

    update/add constants for collect model

--- src/config/constans.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG_STATUS', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split()
