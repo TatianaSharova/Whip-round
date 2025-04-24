44e381e07f1015edfb70ea69a7ed4f564bf18583
Татьяна Шарова <tanyasharova2001@mail.ru>
2025-04-24 08:47:45 +0400

    fix/add schema before documentation

--- src/config/urls.py
+++ src/config/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('swagger/',
+    path('schema/swagger/',
          SpectacularSwaggerView.as_view(url_name='schema'),
          name='swagger'),
-    path('redoc/',
+    path('schema/redoc/',
          SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
